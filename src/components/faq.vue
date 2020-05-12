58e386feb52bd5d3bb234c765dd3945079641b0b
zhaodan <zdailwh@sina.com>
2020-05-12 13:37:40 +0800

    修改 业务网点和常见问题页面的样式

--- src/components/faq.vue
+++ src/components/faq.vue
@@ -1,170 +1,167 @@
 <template>
-	<div class="main">
-		<div class="main_center">
-			<div class="navigation"><p>{{$t("menu.shouye")}}>{{$t("title.faq")}}</p></div>
-			<div class="faq_box">
-				<div class="faq_title"><h4>{{$t("title.faq")}}</h4></div>
-				<ul class="question_box">
-					<li class="question_area">
-						<div class="question_header"><img src="../assets/question.png"><h5 class="question">{{$t("faq.q_jdnzsm")}}</h5></div>
-						<p class="answer show_2line">{{$t("faq.a_jdnzsm")}}</p>
-						<p class="more"><span @click="showAll">{{$t("faq.zhankai")}}<i class="iconfont icon-shousuoxiajiantou"></i></span><span @click="showEllipsis">{{$t("faq.shouqi")}}<i class="iconfont icon-shousuoshangjiantou"></i></span></p>
-					</li>
-					<li class="question_area">
-						<div class="question_header"><img src="../assets/question.png"><h5 class="question">{{$t("faq.q_fwbhgs")}}</h5></div>
-						<p class="answer show_2line">{{$t("faq.a_fwbhgs")}}</p>
-						<p class="more"><span @click="showAll">{{$t("faq.zhankai")}}<i class="iconfont icon-shousuoxiajiantou"></i></span><span @click="showEllipsis">{{$t("faq.shouqi")}}<i class="iconfont icon-shousuoshangjiantou"></i></span></p>
-					</li>
-					<li class="question_area">
-						<div class="question_header"><img src="../assets/question.png"><h5 class="question">{{$t("faq.q_nxbn")}}</h5></div>
-						<p class="answer show_2line">{{$t("faq.a_nxbn")}}</p>
-						<p class="more"><span @click="showAll">{{$t("faq.zhankai")}}<i class="iconfont icon-shousuoxiajiantou"></i></span><span @click="showEllipsis">{{$t("faq.shouqi")}}<i class="iconfont icon-shousuoshangjiantou"></i></span></p>
-					</li>
-					<li class="question_area">
-						<div class="question_header"><img src="../assets/question.png"><h5 class="question">{{$t("faq.q_yfjs")}}</h5></div>
-						<p class="answer show_2line">我们有两种计费规则
-1)	自行联系旅客给出帮带价格，建议不要低于我们平台建议价格。价格不统一，您可以根据物品价值以及时效性为
-2)	通过脚递官方寄件，只需寄到脚递官方地址，会由我们官方委托出行旅客帮您带去物品，省去您的沟通时间，您只需等待我们专人派送人员到达您国家与他联系即可。运费计算可以通过我们网站得出。<router-link to='/fee' style="color:#3EDCFE">详情请点（跳转到运费计算页面）</router-link></p>
-						<p class="more"><span @click="showAll">{{$t("faq.zhankai")}}>></span><span @click="showEllipsis">{{$t("faq.shouqi")}}<a></a></span></p>
-					</li>
-					<li class="question_area">
-						<div class="question_header"><img src="../assets/question.png"><h5 class="question">{{$t("faq.q_dxjd")}}</h5></div>
-						<p class="answer show_2line">{{$t("faq.a_dxjd")}}</p>
-						<p class="more"><span @click="showAll">{{$t("faq.zhankai")}}<i class="iconfont icon-shousuoxiajiantou"></i></span><span @click="showEllipsis">{{$t("faq.shouqi")}}<i class="iconfont icon-shousuoshangjiantou"></i></span></p>
-					</li>
-					<li class="question_area">
-						<div class="question_header"><img src="../assets/question.png"><h5 class="question">{{$t("faq.q_sxx")}}</h5></div>
-						<p class="answer show_2line">{{$t("faq.a_sxx")}}</p>
-						<p class="more"><span @click="showAll">{{$t("faq.zhankai")}}<i class="iconfont icon-shousuoxiajiantou"></i></span><span @click="showEllipsis">{{$t("faq.shouqi")}}<i class="iconfont icon-shousuoshangjiantou"></i></span></p>
-					</li>
-				</ul>
-				<div class="page">
-					<!-- <span class="page_button" v-bind:class="{active: page==1}">&nbsp;</span>
-					<span class="page_button"></span>
-					<span class="page_button"></span>
-					<span class="page_button"></span> -->
-				</div>
-			</div>
-		</div>
-	</div>
+  <div class="main">
+    <div class="main_center">
+      <div class="navigation"><p>{{$t("menu.shouye")}}>{{$t("title.faq")}}</p></div>
+      <div class="faq_box">
+        <div class="faq_title"><h4>{{$t("title.faq")}}</h4></div>
+        <ul class="question_box">
+          <li class="question_area">
+            <div class="question_header"><img src="../assets/question.png"><h5 class="question">{{$t("faq.q_jdnzsm")}}</h5></div>
+            <p class="answer show_2line">{{$t("faq.a_jdnzsm")}}</p>
+            <p class="more"><span @click="showAll">{{$t("faq.zhankai")}}<i class="iconfont icon-shousuoxiajiantou"></i></span><span @click="showEllipsis">{{$t("faq.shouqi")}}<i class="iconfont icon-shousuoshangjiantou"></i></span></p>
+          </li>
+          <li class="question_area">
+            <div class="question_header"><img src="../assets/question.png"><h5 class="question">{{$t("faq.q_fwbhgs")}}</h5></div>
+            <p class="answer show_2line">{{$t("faq.a_fwbhgs")}}</p>
+            <p class="more"><span @click="showAll">{{$t("faq.zhankai")}}<i class="iconfont icon-shousuoxiajiantou"></i></span><span @click="showEllipsis">{{$t("faq.shouqi")}}<i class="iconfont icon-shousuoshangjiantou"></i></span></p>
+          </li>
+          <li class="question_area">
+            <div class="question_header"><img src="../assets/question.png"><h5 class="question">{{$t("faq.q_nxbn")}}</h5></div>
+            <p class="answer show_2line">{{$t("faq.a_nxbn")}}</p>
+            <p class="more"><span @click="showAll">{{$t("faq.zhankai")}}<i class="iconfont icon-shousuoxiajiantou"></i></span><span @click="showEllipsis">{{$t("faq.shouqi")}}<i class="iconfont icon-shousuoshangjiantou"></i></span></p>
+          </li>
+          <li class="question_area">
+            <div class="question_header"><img src="../assets/question.png"><h5 class="question">{{$t("faq.q_yfjs")}}</h5></div>
+            <p class="answer show_2line">我们有两种计费规则
+1)  自行联系旅客给出帮带价格，建议不要低于我们平台建议价格。价格不统一，您可以根据物品价值以及时效性为
+2)  通过脚递官方寄件，只需寄到脚递官方地址，会由我们官方委托出行旅客帮您带去物品，省去您的沟通时间，您只需等待我们专人派送人员到达您国家与他联系即可。运费计算可以通过我们网站得出。<router-link to='/fee' style="color:#3EDCFE">详情请点（跳转到运费计算页面）</router-link></p>
+            <p class="more"><span @click="showAll">{{$t("faq.zhankai")}}>></span><span @click="showEllipsis">{{$t("faq.shouqi")}}<a></a></span></p>
+          </li>
+          <li class="question_area">
+            <div class="question_header"><img src="../assets/question.png"><h5 class="question">{{$t("faq.q_dxjd")}}</h5></div>
+            <p class="answer show_2line">{{$t("faq.a_dxjd")}}</p>
+            <p class="more"><span @click="showAll">{{$t("faq.zhankai")}}<i class="iconfont icon-shousuoxiajiantou"></i></span><span @click="showEllipsis">{{$t("faq.shouqi")}}<i class="iconfont icon-shousuoshangjiantou"></i></span></p>
+          </li>
+          <li class="question_area">
+            <div class="question_header"><img src="../assets/question.png"><h5 class="question">{{$t("faq.q_sxx")}}</h5></div>
+            <p class="answer show_2line">{{$t("faq.a_sxx")}}</p>
+            <p class="more"><span @click="showAll">{{$t("faq.zhankai")}}<i class="iconfont icon-shousuoxiajiantou"></i></span><span @click="showEllipsis">{{$t("faq.shouqi")}}<i class="iconfont icon-shousuoshangjiantou"></i></span></p>
+          </li>
+        </ul>
+        <div class="page">
+          <!-- <span class="page_button" v-bind:class="{active: page==1}">&nbsp;</span>
+          <span class="page_button"></span>
+          <span class="page_button"></span>
+          <span class="page_button"></span> -->
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-	export default{
-		data(){
-			return {
-				page: 1,
-				index: 0,
-			}
-		},
-		methods:{
-			showAll:function(e){
-				let clickDom = e.currentTarget
-				let p = clickDom.parentElement.previousElementSibling
-				p.classList.remove('show_2line')
-				clickDom.style.display = 'none'
-				let span = clickDom.nextElementSibling
-				span.style.display = 'block'
-			},
-			showEllipsis:function(e){
-				let clickDom = e.currentTarget
-				let p = clickDom.parentElement.previousElementSibling
-				p.classList.add('show_2line')
-				clickDom.style.display = 'none'
-				let span = clickDom.previousElementSibling
-				span.style.display = 'block'
-			}
-		}
-	}
+  export default{
+    data(){
+      return {
+        page: 1,
+        index: 0,
+      }
+    },
+    methods:{
+      showAll:function(e){
+        let clickDom = e.currentTarget
+        let p = clickDom.parentElement.previousElementSibling
+        p.classList.remove('show_2line')
+        clickDom.style.display = 'none'
+        let span = clickDom.nextElementSibling
+        span.style.display = 'block'
+      },
+      showEllipsis:function(e){
+        let clickDom = e.currentTarget
+        let p = clickDom.parentElement.previousElementSibling
+        p.classList.add('show_2line')
+        clickDom.style.display = 'none'
+        let span = clickDom.previousElementSibling
+        span.style.display = 'block'
+      }
+    }
+  }
 </script>
 <style scoped>
-	.faq_title{
-		height:33px;
-		font-size:24px;
-		font-weight:500;
-		color:rgba(51,51,51,1);
-		line-height:33px;
-		text-align: left;
-		padding-top: 40px;
-		margin-left: 60px;
-	}
-	.faq_box{
-		background-color: #fff;
-	}
-	.question_header{
-		width:320px;
-		display: flex;
-		margin-bottom: 18px;
-		height: auto;
-	}
-	.question_header img{
-		width: 22px;
-		height: 22px;
-		margin-right: 10px;
-	}
-	.question_area{
-		width: 320px;
-		margin-top: 49px;
-		margin-left: 56px;
-	}
-	.question_box{
-		display: flex;
-		flex-wrap: wrap;
-	}
-	.question{
-		font-size:18px;
-		font-weight:500;
-		color:#000;
-		line-height: 28px;
-		text-align: left;
-	}
-	.answer{
-		font-size:16px;
-		line-height: 25px;
-		height: auto;
-		color:rgba(51,51,51,1);
-		font-weight:400;
-		width:288px;
-		margin-left: 32px;
-		text-align: left;
-	}
-	.show_2line{
-		overflow: hidden;
-		text-align: left;
-		height: 50px;
-		text-overflow: ellipsis;
-		display: -webkit-box;
-		-webkit-line-clamp: 2;
-		-webkit-box-orient: vertical;
-	}
-	.more{
-		width:288px;
-		margin-top: 13px;
-		text-align: left;
-		padding-left: 32px;
-		display: inline-block;
-		height:16px;
-		font-size:16px;
-		font-weight:400;
-		color:rgba(62,220,254,1);
-		line-height:16px;
-		cursor: pointer;
-	}
-	.more span:last-child {
-		display: none;
-	}
-	.page{
-		margin: 24px auto 0px;
-		padding-bottom: 54px;
-	}
-	.page_button{
-		display: inline-block;
-		height: 8px;
-		width: 8px;
-		border-radius: 8px;
-		margin:0px 5px;
-		background-color: rgba(220,220,220,1);
-	}
-	.active{
-		background-color: #1A1A1A;
-	}
+  .faq_title{
+    font-size:24px;
+    line-height:33px;
+    font-weight:500;
+    color:rgba(51,51,51,1);
+    text-align: left;
+    padding: 40px 0 0 40px;
+  }
+  .faq_box{
+    background-color: #fff;
+  }
+  .question_header{
+    width:320px;
+    display: flex;
+    margin-bottom: 18px;
+    height: auto;
+  }
+  .question_header img{
+    width: 22px;
+    height: 22px;
+    margin-right: 10px;
+  }
+  .question_area{
+    width: 320px;
+    margin: 40px 0 0 40px;
+  }
+  .question_box{
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .question{
+    font-size:18px;
+    font-weight:500;
+    color:#000;
+    line-height: 28px;
+    text-align: left;
+  }
+  .answer{
+    font-size:16px;
+    line-height: 25px;
+    height: auto;
+    color:rgba(51,51,51,1);
+    font-weight:400;
+    width:288px;
+    margin-left: 32px;
+    text-align: left;
+  }
+  .show_2line{
+    overflow: hidden;
+    text-align: left;
+    height: 50px;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+  }
+  .more{
+    width:288px;
+    margin-top: 13px;
+    text-align: left;
+    padding-left: 32px;
+    display: inline-block;
+    height:16px;
+    font-size:16px;
+    font-weight:400;
+    color:rgba(62,220,254,1);
+    line-height:16px;
+    cursor: pointer;
+  }
+  .more span:last-child {
+    display: none;
+  }
+  .page{
+    margin: 24px auto 0px;
+    padding-bottom: 54px;
+  }
+  .page_button{
+    display: inline-block;
+    height: 8px;
+    width: 8px;
+    border-radius: 8px;
+    margin:0px 5px;
+    background-color: rgba(220,220,220,1);
+  }
+  .active{
+    background-color: #1A1A1A;
+  }
 </style>
--- src/components/shopAddress.vue
+++ src/components/shopAddress.vue
@@ -1,131 +1,187 @@
 <template>
-	<div class="main">
-		<div class="main_center">
-			<div class="navigation"><p>{{$t("menu.shouye")}}>{{$t("title.yewu")}}</p></div>
-			<div class="yw_box">
-				<table class="yw_ul">
-					<tr class="yw_main">
-						<td class="yw_logo"><img src="../assets/chinese.png"></td>
-						<td class="yw_country">中国</td>
-						<td class="yw_city">北京</td>
-						<td class="yw_intro">
-							<p class="yw_address">{{$t("yewu.dizhi")}}:朝阳区建华南路19号楼长安6号</p>
-							<p class="yw_address">{{$t("yewu.dianhua")}}:18518501618</p>
-							<p class="yw_address">{{$t("yewu.lianxiren")}}:池诗诗 </p>
-							<p class="yw_address">{{$t("yewu.youbian")}}:100022</p>
-						</td>
-						<td></td>
-						<td></td>
-					</tr>
-					<tr class="yw_main">
-						<td class="yw_logo"><img src="../assets/russia.png"></td>
-						<td class="yw_country">俄罗斯</td>
-						<td class="yw_city">圣彼得堡</td>
-						<td class="yw_intro">
-							<p class="yw_address">{{$t("yewu.dizhi")}}:улица Фёдора Абрамова, дом 23, корпус 1, город Санкт-Петербург</p>
-							<p class="yw_address">{{$t("yewu.dianhua")}}:+7(921)599-16-84</p>
-							<p class="yw_address">{{$t("yewu.lianxiren")}}:максим хопин </p>
-							<p class="yw_address">{{$t("yewu.youbian")}}:194358</p>
-						</td>
-						<td class="yw_city">叶卡捷琳堡</td>
-						<td class="yw_intro">
-							<p class="yw_address">{{$t("yewu.dizhi")}}:улица Большакова, 79 Екатеринбург</p>
-							<p class="yw_address">{{$t("yewu.dianhua")}}:+7 (982) 662-77-89</p>
-							<p class="yw_address">{{$t("yewu.lianxiren")}}:李哲凯 </p>
-							<p class="yw_address">{{$t("yewu.youbian")}}:620 000</p>
-						</td>
-					</tr>
-					<tr class="yw_main">
-						<td class="yw_logo"><img src="../assets/usa.png"></td>
-						<td class="yw_country">美国</td>
-						<td class="yw_city">纽约</td>
-						<td class="yw_intro">
-							<p class="yw_address">{{$t("yewu.dizhi")}}:120 Meyer Road apartment 107, buffalo, New York, US</p>
-							<p class="yw_address">{{$t("yewu.dianhua")}}:+1716-465-0550</p>
-							<p class="yw_address">{{$t("yewu.lianxiren")}}:Koro</p>
-							<p class="yw_address">{{$t("yewu.youbian")}}:14226</p>
-						</td>
-						<td></td>
-						<td></td>
-					</tr>
-					<tr class="yw_main">
-						<td class="yw_logo"><img src="../assets/malaixiya.png"></td>
-						<td class="yw_country">马来西亚</td>
-						<td class="yw_city">槟城</td>
-						<td class="yw_intro" colspan="3">
-							<p class="yw_address">{{$t("yewu.dizhi")}}:58, Lorong Rajawali 13, Taman Berjaya, 14300 nibong tebal, Pulau Pinang, Malaysia</p>
-							<p class="yw_address">{{$t("yewu.dianhua")}}:+60 0125685715</p>
-							<p class="yw_address">{{$t("yewu.lianxiren")}}:Lee peiying </p>
-							<p class="yw_address">{{$t("yewu.youbian")}}:14300</p>
-						</td>
-					</tr>
-					<tr class="yw_main">
-						<td class="yw_logo"><img src="../assets/eng.png"></td>
-						<td class="yw_country">英国</td>
-						<td class="yw_city">伦敦</td>
-						<td class="yw_intro">
-							<p class="yw_address">{{$t("yewu.dizhi")}}:Gabor Hall (Central)Princes Gardens London United Kingdom</p>
-							<p class="yw_address">{{$t("yewu.dianhua")}}:+44 7529938866</p>
-							<p class="yw_address">{{$t("yewu.lianxiren")}}:Cosmos</p>
-							<p class="yw_address">{{$t("yewu.youbian")}}:SW7 1AZ</p>
-						</td>
-						<td class="yw_city">曼切斯顿</td>
-						<td>
-							<p class="yw_address">{{$t("yewu.dizhi")}}:Upper brook st,liberty park,f14.04</p>
-							<p class="yw_address">{{$t("yewu.dianhua")}}:+44 7852427680</p>
-							<p class="yw_address">{{$t("yewu.lianxiren")}}:David</p>
-							<p class="yw_address">{{$t("yewu.youbian")}}:M13 0FZ</p>
-						</td>
-					</tr>
-				</table>
-			</div>
-		</div>
-	</div>
+  <div class="main">
+    <div class="main_center">
+      <div class="navigation"><p>{{$t("menu.shouye")}}>{{$t("title.yewu")}}</p></div>
+      <div class="yw_box">
+        <table class="yw_ul">
+          <tr class="yw_main">
+            <td class="yw_logo"><img src="../assets/chinese.png"></td>
+            <td class="yw_country">中国</td>
+            <td class="yw_city">北京</td>
+            <td class="yw_intro">
+              <p>{{$t("yewu.dizhi")}}:朝阳区建华南路19号楼长安6号</p>
+              <p>{{$t("yewu.dianhua")}}:18518501618</p>
+              <p>{{$t("yewu.lianxiren")}}:池诗诗 </p>
+              <p>{{$t("yewu.youbian")}}:100022</p>
+            </td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr class="yw_main">
+            <td class="yw_logo"><img src="../assets/russia.png"></td>
+            <td class="yw_country">俄罗斯</td>
+            <td class="yw_city">圣彼得堡</td>
+            <td class="yw_intro">
+              <p>{{$t("yewu.dizhi")}}:улица Фёдора Абрамова, дом 23, корпус 1, город Санкт-Петербург</p>
+              <p>{{$t("yewu.dianhua")}}:+7(921)599-16-84</p>
+              <p>{{$t("yewu.lianxiren")}}:максим хопин </p>
+              <p>{{$t("yewu.youbian")}}:194358</p>
+            </td>
+            <td class="yw_city hideOnPhone-td">叶卡捷琳堡</td>
+            <td class="yw_intro hideOnPhone-td">
+              <p>{{$t("yewu.dizhi")}}:улица Большакова, 79 Екатеринбург</p>
+              <p>{{$t("yewu.dianhua")}}:+7 (982) 662-77-89</p>
+              <p>{{$t("yewu.lianxiren")}}:李哲凯 </p>
+              <p>{{$t("yewu.youbian")}}:620 000</p>
+            </td>
+            <td class="showOnPhone-td"></td>
+            <td class="showOnPhone-td"></td>
+          </tr>
+          <tr class="yw_main showOnPhone-tr">
+            <td></td>
+            <td></td>
+            <td class="yw_city">叶卡捷琳堡</td>
+            <td class="yw_intro">
+              <p>{{$t("yewu.dizhi")}}:улица Большакова, 79 Екатеринбург</p>
+              <p>{{$t("yewu.dianhua")}}:+7 (982) 662-77-89</p>
+              <p>{{$t("yewu.lianxiren")}}:李哲凯 </p>
+              <p>{{$t("yewu.youbian")}}:620 000</p>
+            </td>
+            <td class="showOnPhone-td"></td>
+            <td class="showOnPhone-td"></td>
+          </tr>
+          <tr class="yw_main">
+            <td class="yw_logo"><img src="../assets/usa.png"></td>
+            <td class="yw_country">美国</td>
+            <td class="yw_city">纽约</td>
+            <td class="yw_intro">
+              <p>{{$t("yewu.dizhi")}}:120 Meyer Road apartment 107, buffalo, New York, US</p>
+              <p>{{$t("yewu.dianhua")}}:+1716-465-0550</p>
+              <p>{{$t("yewu.lianxiren")}}:Koro</p>
+              <p>{{$t("yewu.youbian")}}:14226</p>
+            </td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr class="yw_main">
+            <td class="yw_logo"><img src="../assets/malaixiya.png"></td>
+            <td class="yw_country">马来西亚</td>
+            <td class="yw_city">槟城</td>
+            <td class="yw_intro" colspan="3">
+              <p>{{$t("yewu.dizhi")}}:58, Lorong Rajawali 13, Taman Berjaya, 14300 nibong tebal, Pulau Pinang, Malaysia</p>
+              <p>{{$t("yewu.dianhua")}}:+60 0125685715</p>
+              <p>{{$t("yewu.lianxiren")}}:Lee peiying </p>
+              <p>{{$t("yewu.youbian")}}:14300</p>
+            </td>
+          </tr>
+          <tr class="yw_main">
+            <td class="yw_logo"><img src="../assets/eng.png"></td>
+            <td class="yw_country">英国</td>
+            <td class="yw_city">伦敦</td>
+            <td class="yw_intro">
+              <p>{{$t("yewu.dizhi")}}:Gabor Hall (Central)Princes Gardens London United Kingdom</p>
+              <p>{{$t("yewu.dianhua")}}:+44 7529938866</p>
+              <p>{{$t("yewu.lianxiren")}}:Cosmos</p>
+              <p>{{$t("yewu.youbian")}}:SW7 1AZ</p>
+            </td>
+            <td class="yw_city hideOnPhone-td">曼切斯顿</td>
+            <td class="yw_intro hideOnPhone-td">
+              <p>{{$t("yewu.dizhi")}}:Upper brook st,liberty park,f14.04</p>
+              <p>{{$t("yewu.dianhua")}}:+44 7852427680</p>
+              <p>{{$t("yewu.lianxiren")}}:David</p>
+              <p>{{$t("yewu.youbian")}}:M13 0FZ</p>
+            </td>
+            <td class="showOnPhone-td"></td>
+            <td class="showOnPhone-td"></td>
+          </tr>
+          <tr class="yw_main showOnPhone-tr">
+            <td></td>
+            <td></td>
+            <td class="yw_city">曼切斯顿</td>
+            <td class="yw_intro">
+              <p>{{$t("yewu.dizhi")}}:Upper brook st,liberty park,f14.04</p>
+              <p>{{$t("yewu.dianhua")}}:+44 7852427680</p>
+              <p>{{$t("yewu.lianxiren")}}:David</p>
+              <p>{{$t("yewu.youbian")}}:M13 0FZ</p>
+            </td>
+            <td class="showOnPhone-td"></td>
+            <td class="showOnPhone-td"></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-	.yw_box{
-		background-color: #fff;
-	}
-	.yw_ul{
-		width: 100%;
-	}
-	.yw_main{
-		text-align: left;
-		line-height:26px;
-		height: 153px;
-		border-bottom: 1px solid #DCDCDC;
-		width: 100%;
-	}
-	.yw_logo{
-		width: 90px;
-		text-align: center;
-	}
-	.yw_logo img{
-		width: 40px;
-		margin-left: 30px;
-		margin-right: 20px;
-	}
-	.yw_intro{
-		width: 430px;
-	}
-	.yw_country{
-		font-size: 16px;
-		color: #1A1A1A;
-		font-weight:500;
-		width: 70px;
-		text-align: center;
-	}
-	.yw_city{
-		font-size: 14px;
-		color: #888;
-		font-weight:500;
-		width: 84px;
-		padding-left: 20px;
-	}
-	.yw_address{
-		font-size: 14px;
-		color: #888;
-		font-weight:500;
-	}
+  .yw_box{
+    background-color: #fff;
+  }
+  .yw_ul{
+    width: 100%;
+  }
+  .yw_main{
+    width: 100%;
+    text-align: left;
+    font-size: 14px;
+    line-height:26px;
+    color: #888;
+    font-weight:500;
+    border-bottom: 1px solid #DCDCDC;
+  }
+  .yw_logo{
+    width: 8%;
+    text-align: center;
+  }
+  .yw_logo img{
+    width: 50%;
+  }
+  .yw_country{
+    width: 8%;
+    font-size: 16px;
+    color: #1A1A1A;
+    text-align: center;
+  }
+  .yw_city{
+    width: 10%;
+    padding: 0 10px;
+  }
+  .yw_intro{
+    padding: 15px 0;
+  }
+  .showOnPhone-tr {
+    display: none;
+  }
+  .showOnPhone-td {
+    display: none;
+  }
+  .hideOnPhone-td {
+    display: table-cell;
+  }
+
+  @media (max-width: 981px){
+    .showOnPhone-tr {
+      display: table-row;
+    }
+    .showOnPhone-td {
+      display: table-cell;
+    }
+    .hideOnPhone-td {
+      display: none;
+    }
+    .yw_main{
+      font-size: .4rem;
+      line-height: .6rem;
+    }
+    .yw_logo{
+      width: 12%;
+    }
+    .yw_country{
+      width: 15%;
+      font-size: .42rem;
+    }
+    .yw_city{
+      width: 15%;
+    }
+  }
 </style>
